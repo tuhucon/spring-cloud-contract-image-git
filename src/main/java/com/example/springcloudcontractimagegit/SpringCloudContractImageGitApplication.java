1ab6bab1cf590b9f259bf2d4cdd769c259dcfabf
nguyen minh tuan <minhtuan93@yahoo.com>
2019-08-13 10:38:52 +0700

    add stub

---
+++ src/main/java/com/example/springcloudcontractimagegit/SpringCloudContractImageGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudcontractimagegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractImageGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudContractImageGitApplication.class, args);
+    }
+
+}
